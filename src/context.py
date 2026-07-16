d11adf10c7f90816ec584fa0abd887f7492875d7
RenatoRodri-Sec <renatomrodri01@gmail.com>
2026-07-16 11:19:15 -0300

    Update context.py

--- src/context.py
+++ src/context.py
@@ -1,1 +1,15 @@
+class ConversationContext:
+    def __init__(self):
+        self.history = []
 
+    def add_message(self, user, message):
+        self.history.append({
+            "user": user,
+            "message": message
+        })
+
+    def get_history(self):
+        return self.history
+
+    def clear(self):
+        self.history = []
